939f3686a0f0de4b719d769964f08da2173279f2
MAON <durraizbukhari1234@gmail.com>
2025-02-25 23:49:40 +0500

    strings

---
+++ thriftify_fyp_1/lib/utils/constants/image_strings.dart
@@ -0,0 +1,11 @@
+
+class TImages{
+  // App logos
+  static const String darkApplogo = 'assets/images/logo.png';
+  static const String lightApplogo = 'assets/images/logo.png';
+
+  //social logos
+  static const String facebook = 'assets/images/facebook.png';
+  static const String google = 'assets/images/instagram.png';
+  
+}
---
+++ thriftify_fyp_1/lib/utils/constants/text_strings.dart
@@ -0,0 +1,12 @@
+class TTexts {
+  static const String onBoardingTitle1 = "Choose Your Product";
+  static const String onBoardingTitle2 = "Have a look on the products";
+  static const String onBoardingTitle3 = "Thriftify your clothes";
+
+  static const String onBoardingSubTitle1 = "Welcome to World of limitless choices - Your perfect order awaits";
+  static const String onBoardingSubTitle2 = "Hurry Up new products are listed";
+  static const String onBoardingSubTitle3 = "From seller to buyer - OLX for clothes";
+
+  static const String homeAppbarTitle = "Good Day for Shopping";
+  static const String homeAppbarSubTitle = "Durraiz Ali";
+}
