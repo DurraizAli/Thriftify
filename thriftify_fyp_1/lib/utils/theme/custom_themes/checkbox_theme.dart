ae89ff73db8166d745212f188dc866b25b571fe8
MAON <durraizbukhari1234@gmail.com>
2025-04-19 22:38:33 +0500

    checkbox_theme

--- thriftify_fyp_1/lib/utils/theme/custom_themes/checkbox_theme.dart
+++ thriftify_fyp_1/lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -1,5 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:thriftify_fyp_1/utils/constants/colors.dart';
+import 'package:thriftify_fyp_1/utils/constants/sizes.dart';
 
+// class TCheckboxTheme {
+//   TCheckboxTheme._(); // To avoid creating instances
+//   /// Customizable Light Text Theme
+//   static CheckboxThemeData lightCheckboxTheme = CheckboxThemeData(
+//     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+//     checkColor: MaterialStateProperty.resolveWith((states) {
+//       if (states.contains(MaterialState.selected)) {
+//         return Colors.white;
+//       } else {
+//         return Colors.black;
+//       }
+//     }),
+//     fillColor: MaterialStateProperty.resolveWith((states) {
+//       if (states.contains(MaterialState.selected)) {
+//         return Colors.blue;
+//       } else {
+//         return Colors.transparent;
+//       }
+//     }),
+//   ); // CheckboxThemeData
+//   /// Customizable Dark Text Theme
+//   static CheckboxThemeData darkCheckboxTheme = CheckboxThemeData(
+//     shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+//     checkColor: MaterialStateProperty.resolveWith((states) {
+//       if (states.contains(MaterialState.selected)) {
+//         return Colors.white;
+//       } else {
+//         return Colors.black;
+//       }
+//     }),
+//     fillColor: MaterialStateProperty.resolveWith((states) {
+//       if (states.contains(MaterialState.selected)) {
+//         return Colors.blue;
+//       } else {
+//         return Colors.transparent;
+//       }
+//     }),
+//   ); // CheckboxTheme Data
+// }
+// class TCheckboxTheme {
+//   TCheckboxTheme._();
+
+//   static CheckboxThemeData lightCheckboxTheme = CheckboxThemeData(
+//     shape:
+//         RoundedRectangleBorder(borderRadius: BorderRadius.circular(TSizes.xs)),
+//     checkColor: MaterialStateProperty.resolveWith((states) {
+//       if (states.contains(MaterialState.selected)) {
+//         return TColors.white;
+//       } else {
+//         return TColors.black;
+//       }
+//     }),
+//     fillColor: MaterialStateProperty.resolveWith((states) {
+//       if (states.contains(MaterialState.selected)) {
+//         return TColors.primary;
+//       } else {
+//         return Colors.transparent;
+//       }
+//     }),
+//   );
+// }
 class TCheckboxTheme {
   TCheckboxTheme._(); // To avoid creating instances
   /// Customizable Light Text Theme
