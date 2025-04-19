a26f13bbafab36f9e1a87374deec3ffaf426fa89
XVARVE <shah.momin.haider.10@gmail.com>
2025-04-20 00:55:24 +0500

    Horizontal List View

--- thriftify_fyp_1/lib/features/shop/screens/home/widgets/home_categories.dart
+++ thriftify_fyp_1/lib/features/shop/screens/home/widgets/home_categories.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/instance_manager.dart';
 import 'package:thriftify_fyp_1/features/shop/screens/home/widgets/image_text_widgets/vertiacl_image_text.dart';
+import 'package:thriftify_fyp_1/features/shop/screens/sub_category/sub_categories.dart';
 import 'package:thriftify_fyp_1/utils/constants/image_strings.dart';
 
 class THomeCategories extends StatelessWidget {
@@ -16,7 +19,7 @@ class THomeCategories extends StatelessWidget {
         itemCount: 6,
         scrollDirection: Axis.horizontal,
         itemBuilder: (_, index) {
-          return  TVerticalImageText(image: TImages.facebook, title: 'Shoes', onTap:(){} , );
+          return  TVerticalImageText(image: TImages.facebook, title: 'Shoes', onTap:() => Get.to(()=> const SubCategoriesScreen()) , );
       
         }),
     );
---
+++ thriftify_fyp_1/lib/features/shop/screens/sub_category/sub_categories.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:thriftify_fyp_1/common/widgets/appbar/appbar.dart';
+import 'package:thriftify_fyp_1/common/widgets/images/t_rounded_image.dart';
+import 'package:thriftify_fyp_1/common/widgets/products/product_cards/product_card_horizontal.dart';
+import 'package:thriftify_fyp_1/common/widgets/texts/section_heading.dart';
+import 'package:thriftify_fyp_1/utils/constants/image_strings.dart';
+import 'package:thriftify_fyp_1/utils/constants/sizes.dart';
+
+class SubCategoriesScreen extends StatelessWidget {
+  const SubCategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const TAppBar(title: Text('Sports'), showBackArrow: true,),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              const TRoundedImage(width: double.infinity, imageUrl: TImages.promoBanner1, applyImageRadius: true,),
+              const SizedBox(height: TSizes.spaceBtwSections,),
+
+
+              Column(
+                children: [
+                  TSectionHeading(title: 'Sports Shirts', onPressed: (){},),
+                  const SizedBox(height:  TSizes.spaceBtwItems / 2),
+
+                  SizedBox(
+                    height: 120,
+                    child: ListView.separated(
+                      itemCount: 4,
+                      scrollDirection: Axis.horizontal,
+                      separatorBuilder: (context, index) => const SizedBox(width: TSizes.spaceBtwItems),
+                      itemBuilder: (context, index) => const TProductCardHorizontal(),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),),
+      ),
+    );
+  }
+}
