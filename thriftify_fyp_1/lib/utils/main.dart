f9231316ed730054c89d86a14eeef78503884149
XVARVE <shah.momin.haider.10@gmail.com>
2025-04-20 17:29:46 +0500

    Firebase Setup

--- thriftify_fyp_1/lib/utils/main.dart
+++ thriftify_fyp_1/lib/utils/main.dart
@@ -1,7 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:thriftify_fyp_1/app.dart';
+import 'package:thriftify_fyp_1/firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform).then((FirebaseApp value) => Get.put
+    );
+
   
   runApp(const App());
 }
